e789f81e652d6928c30c51c42135d579f2c2e274
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-10-03 09:43:32 -0500

    Initial Commit

---
+++ common/src/main/java/org/valkyrienskies/addon/ExampleMod.java
@@ -0,0 +1,9 @@
+package org.valkyrienskies.addon;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "vs_addon";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
